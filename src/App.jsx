ed6d4fd00af986c613ff10bea6045b8afce54ba0
Travis Anderson <travis@TravissiMac842.hsd1.co.comcast.net>
2025-03-02 11:17:46 -0700

    adding tailwind to css and clearing out the app component

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,9 @@ import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+  const [ride, setRide] = useState(null);
+  const [rides, setRides] = useState(null);
+  const [rideStats, setRideStats] = useState(null);
 
   return (
     <main className="container">
@@ -29,21 +25,7 @@ function App() {
       </div>
       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
+      <p>Ebike</p>
     </main>
   );
 }
